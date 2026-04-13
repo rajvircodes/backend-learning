8581f53f14db42c51f65368e2a93dc8f357be0fc
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-04-13 15:05:25 +0530

    initial commit

---
+++ 05-traversy-backend/backend/server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const goalRoutes = require('./routes/goals.routes')
+const {errorHandler} = require('./middleware/errorMiddleware')
+
+
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+app.use('/api/goals', goalRoutes)
+
+const port = process.env.PORT || 3000
+
+app.use(errorHandler)
+
+
+app.listen(port, () => console.log("Server is running on port 3000"));
