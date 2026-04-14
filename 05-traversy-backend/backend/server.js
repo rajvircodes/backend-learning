e3ef5ce4141e5f745687cc077fed8eea844cd0fb
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-04-14 08:34:35 +0530

    databases connection

--- 05-traversy-backend/backend/server.js
+++ 05-traversy-backend/backend/server.js
@@ -1,8 +1,12 @@
+require('dotenv').config()
 const express = require("express");
 const app = express();
 const goalRoutes = require('./routes/goals.routes')
 const {errorHandler} = require('./middleware/errorMiddleware')
+const connectDB = require('./config/db')
 
+
+connectDB()
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
