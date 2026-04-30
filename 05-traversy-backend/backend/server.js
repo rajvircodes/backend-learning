3d7a1f524231feb9fe36f69e46c1dee68310d394
rajvircodes <rajvirsinhgohil004@gmail.com>
2026-04-30 13:51:27 +0530

    goal model changes

--- 05-traversy-backend/backend/server.js
+++ 05-traversy-backend/backend/server.js
@@ -2,6 +2,7 @@ require('dotenv').config()
 const express = require("express");
 const app = express();
 const goalRoutes = require('./routes/goals.routes')
+const userRoutes = require('./routes/user.routes')
 const {errorHandler} = require('./middleware/errorMiddleware')
 const connectDB = require('./config/db')
 const cookieParser = require('cookie-parser')
@@ -11,6 +12,7 @@ app.use(express.json())
 app.use(cookieParser())
 app.use(express.urlencoded({extended:true}))
 app.use('/api/goals', goalRoutes)
+app.use('/api/users', userRoutes)
 
 const port = process.env.PORT || 3000
 
